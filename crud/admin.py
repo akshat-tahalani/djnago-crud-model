1def66984edf81a956f4c67bbf27bbcca1b3519a
Akshat tahalani <tahalani.akshat@gmail.com>
2026-05-18 03:37:22 +0530

    add crud opertaions

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User , Course ,Lesson ,Instructor
+
+
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Instructor)
+# this acommand ios spexcifc to djangos admin panel to show 
+#that data is managed using djangos auto genetrated data using uits ui
+
